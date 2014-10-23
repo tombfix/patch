32269edd0966b493f3de1011f56e81fd8f9bc1d6
Syoichi Tsuyuhara <syoichi@outlook.com>
2014-10-24 05:20:00 +0900

    Remove patches that don't work

--- action.clearCache.js
+++
@@ -1,7 +0,0 @@
-Tombloo.Service.actions.register({
-	name : 'Clear Cache',
-	icon : 'chrome://tombloo/skin/firefox.ico',
-	execute : function(){
-		CacheService.evictEntries(ICache.STORE_ANYWHERE);
-	},
-}, '----');
--- action.copyInstalledExtensions.js
+++
@@ -1,9 +0,0 @@
-Tombloo.Service.actions.register({
-	name : 'Copy Installed Extensions',
-	icon : 'chrome://tombloo/skin/firefox.ico',
-	execute : function(){
-    copyString(FuelApplication.extensions.all.reduce(function(m, ext) {
-      return m + (ext.enabled ? [ext.name, ext.version].join(" ") + "\n" : '');
-    }, ''));
-	},
-}, '----');
