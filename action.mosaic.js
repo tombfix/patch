dda90ce6015c9d2caed045bce74b91f0525c6fb3
Syoichi Tsuyuhara <syoichi@outlook.com>
2014-10-23 16:15:16 +0900

    Remove Tombloo's old default features
    FYI: https://github.com/to/tombloo/commit/25188f4f19a70083ad7a52c425776f67478d09c0
         https://github.com/to/tombloo/commit/b7cde04bd201c44bd93b7758982e9ace53f8cf29
    
    This features no longer work.
    Goodbye, Strobo, Mosaic, "Download All Posts and Photos" Action!

--- action.mosaic.js
+++
@@ -1,7 +0,0 @@
-Tombloo.Service.actions.register({
-	type : 'menu',
-	name : 'Mosaic',
-	execute : function(){
-		addTab('chrome://tombloo-patch/content/script/Mosaic.html');
-	},
-}, '----');
--- action.strobo.js
+++
@@ -1,7 +0,0 @@
-Tombloo.Service.actions.register(	{
-	name : 'Strobo',
-	execute : function(){
-		// FIXME: patch‚ðchrome‚É’Ç‰Á‚µˆÚ“®
-		addTab('chrome://tombloo/content/library/Strobo.html');
-	},
-}, '----');
