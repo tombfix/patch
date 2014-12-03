0724c34abea2d4d5d9e3379a053401a84c7601f1
Syoichi Tsuyuhara <syoichi@outlook.com>
2014-12-03 22:44:58 +0900

    Revise "Send to Instapaper" Action
    * Fix icon

--- action.sendToInstapaper.js
+++ action.sendToInstapaper.js
@@ -1,14 +1,14 @@
-Tombloo.Service.actions.register({
-	name : 'Send to Instapaper',
-	type : 'context',
-	icon : 'chrome://tombloo/skin/instapaper.ico',
-	execute : function(ctx){
-		return models.Instapaper.post(ctx.onLink? {
-			item : ctx.link.textContent,
-			itemUrl : ctx.link.href,
-		} : {
-			item : ctx.title,
-			itemUrl : ctx.href,
-		});
-	},
+Actions.register({
+  name : 'Send to Instapaper',
+  type : 'context',
+  icon : Instapaper.ICON,
+  execute : function (ctx) {
+    return Instapaper.post(ctx.onLink ? {
+      item    : ctx.link.textContent,
+      itemUrl : ctx.link.href
+    } : {
+      item    : ctx.title,
+      itemUrl : ctx.href
+    });
+  }
 }, '----');
